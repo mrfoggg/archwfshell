5ca71bfff2eac5385a9102a9ccb60d238459758a
thinkpad-fedora <mrfoggg@gmail.com>
2025-05-28 22:25:58 +0300

    if not test -d wf-shell

--- wf-shell-build.sh
+++ wf-shell-build.sh
@@ -1,5 +1,10 @@
 cd ~/installs-tmp
-git clone https://github.com/WayfireWM/wf-shell && cd wf-shell
+
+if not test -d wf-shell
+    git clone https://github.com/WayfireWM/wf-shell
+end
+
+cd wf-shell
 
 set -x PKG_CONFIG_PATH /usr/lib/wlroots0.17/pkgconfig /usr/lib/pkgconfig /usr/share/pkgconfig
 meson build --prefix=/usr --buildtype=release
