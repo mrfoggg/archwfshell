a08e8432a6550db41f16302418ba540911f6853a
thinkpad-fedora <mrfoggg@gmail.com>
2025-05-28 23:01:33 +0300

    wf ninja

--- wf-shell-build.sh
+++ wf-shell-build.sh
@@ -8,3 +8,4 @@ cd wf-shell
 
 set -x PKG_CONFIG_PATH /usr/lib/wlroots0.17/pkgconfig /usr/lib/pkgconfig /usr/share/pkgconfig
 meson build --prefix=/usr --buildtype=release
+ninja -C build && sudo ninja -C build install
