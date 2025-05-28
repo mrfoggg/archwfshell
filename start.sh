efa2a5d81835608be3c083cc910b3434689de916
thinkpad-fedora <mrfoggg@gmail.com>
2025-05-28 18:31:11 +0300

    first commit

---
+++ start.sh
@@ -0,0 +1,7 @@
+sudo pacman -S micro bat btop alacritty make gtkmm3 gtk-layer-shell libdbusmenu-gtk3 glib2-devel
+
+mkdir ~/installs-tmp
+cd ~/installs-tmp
+
+git clone https://aur.archlinux.org/yay.git && cd yay
+makepkg -si
